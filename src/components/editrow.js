3e539395019db9713e6f89641d6c60fc81025b45
LouisRobbins96 <louis.robbins96@outlook.com>
2022-02-07 16:55:28 +0000

    more css changes

--- src/components/editrow.js
+++ src/components/editrow.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const EditRow = ({ editTableData, handleEditTableChange}) => {
+const EditRow = ({ editTableData, handleEditTableChange }) => {
     return (
         <tr>
             <td>
